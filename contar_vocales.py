198d189f30c39b34deda48cf1b248c8a6926c4bd
jesusa-sanchez-git <jesusa.sanchez@iteso.mx_PianoMan11VJ>
2025-03-21 08:32:43 -0600

    Lo modifique para acentos

--- contar_vocales.py
+++ contar_vocales.py
@@ -2,7 +2,7 @@ Frase = input("Ingrese una frase : \n")
 frasemin = Frase.lower()
 i =0
 for vocales in frasemin:
-    if vocales== "a" or vocales== "e" or vocales== "i" or vocales== "o" or vocales== "u":
+    if vocales== "a" or vocales== "e" or vocales== "i" or vocales== "o" or vocales== "u" or vocales== "á" or vocales== "é" or vocales== "í" or vocales== "ó" or vocales== "ú":
         i+=1
         True
 print(f"La frase tiene {i} vocales")
