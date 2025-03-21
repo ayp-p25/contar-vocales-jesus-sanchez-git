8728324b0b98dbdcfc536db2e8fdf3f8820ad433
jesusa-sanchez-git <jesusa.sanchez@iteso.mx_PianoMan11VJ>
2025-03-21 08:29:18 -0600

    First

--- contar_vocales.py
+++ contar_vocales.py
@@ -1,15 +1,8 @@
-"""
-Inserta el encabezado aquí y escribe tu código abajo
-"""
-
-# Declaraciones
-CONSTANTE = valor
-
-# Entradas
-entrada = input()
-
-# Proceso
-
-
-# Salidas
-print(salida)
+Frase = input("Ingrese una frase : \n")
+frasemin = Frase.lower()
+i =0
+for vocales in frasemin:
+    if vocales== "a" or vocales== "e" or vocales== "i" or vocales== "o" or vocales== "u":
+        i+=1
+        True
+print(f"La frase tiene {i} vocales")
